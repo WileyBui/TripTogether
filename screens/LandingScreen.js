f62b947c4316c60e509bb7cc9d8e634a07bbd22e
Braeden Haesemeyer <haese006@umn.edu>
2020-04-01 17:57:10 -0500

    Update LandingScreen.js

--- screens/LandingScreen.js
+++ screens/LandingScreen.js
@@ -7,8 +7,10 @@ import {
   TouchableHighlight
 } from "react-native";
 import { HeaderComponent } from "../components/ScreenHeader";
+
 export default function LandingScreen(props) {
   return (
+
     <View style={styles.container}>
       {/* Don't delete this: This is the header component and
          you need it to navigate to other screens */}
@@ -17,18 +19,25 @@ export default function LandingScreen(props) {
       <Text style={styles.header}>Plan a TripTogether</Text>
 
 
-
       <TouchableHighlight
-        // onPress={loginButtonPressed()}
-        onPress={() => props.setShowLogin(true)}
+        onPress={console.log("Button Pressed")}
+        style={styles.button}
+      >
+      <TouchableHighlight
+         onPress={loginButtonPressed()}
+        //onPress={() => props.setShowLogin(true)}
         style={styles.buttonAlt}
       >
         <Text style={styles.buttonTextAlt}>Sign In</Text>
       </TouchableHighlight>
-
+      <View style={{ alignItems: "center", marginTop: 60 }}>
+        <Text style={{ color: "#032224", fontWeight: "bold" }}>
+          Don't have an Account?
+        </Text>
+      </View>
       <TouchableHighlight
-        // onPress={loginButtonPressed()}
-        onPress={() => props.setShowLogin(false)}
+         onPress={loginButtonPressed()}
+        //nPress={() => props.setShowLogin(false)}
         style={styles.buttonAlt}
       >
         <Text style={styles.buttonTextAlt}>Sign Up</Text>
@@ -40,9 +49,48 @@ export default function LandingScreen(props) {
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: "10%",
+    margin: 20,
     flex: 1,
-    backgroundColor: "#fafafa",
-    alignItems: "center"
+    backgroundColor: "#fff"
+  },
+  input: {
+    margin: 10,
+    height: 50,
+    borderColor: "#032224",
+    borderWidth: 2,
+    borderRadius: 25,
+    paddingLeft: 20
+  },
+  button: {
+    margin: 10,
+    marginTop: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "95%",
+    height: 50,
+    backgroundColor: "#032224",
+    borderColor: "#032224",
+    // borderWidth: 2,
+    borderRadius: 25
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold"
+  },
+  buttonAlt: {
+    margin: 10,
+    marginTop: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "95%",
+    height: 50,
+    backgroundColor: "#fff",
+    borderColor: "#032224",
+    borderWidth: 3,
+    borderRadius: 25
+  },
+  buttonTextAlt: {
+    color: "#032224",
+    fontWeight: "bold"
   }
 });
