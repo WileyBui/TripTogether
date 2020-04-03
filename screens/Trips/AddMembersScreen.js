36e42bab51389eec9a8ce92467f5854b2b6a40ef
Romo <annurul.islam@bluecrossmn.com>
2020-04-03 16:11:21 -0500

    Added search input

--- screens/Trips/AddMembersScreen.js
+++ screens/Trips/AddMembersScreen.js
@@ -5,50 +5,68 @@ import {
   Text,
   View,
   Image,
-  TextInput,
+  TextInput
 } from "react-native";
 import { HeaderComponent } from "../../components/ScreenHeader";
+import { Ionicons } from "@expo/vector-icons";
+
 export default function AddMembersSreen(props) {
   return (
     <View style={styles.container}>
       {/* Don't delete this: This is the header component and
          you need it to navigate to other screens */}
       <HeaderComponent headerProps={props} screenTitle="Add Member" />
-      <TextInput
-      style={styles.input5}
-      placeholder="Search your member"
-      placeholderTextColor="#032224"
-      />
+
+      <View
+        style={{
+          flexDirection: "row",
+          padding: 30
+        }}
+      >
+        <View style={styles.leftComponent}>
+          <Text>
+            <Ionicons name="ios-search" size={20} color="black" />
+          </Text>
+        </View>
+        <View style={styles.rightComponent}>
+          <Text>Search for Member</Text>
+        </View>
+
+        {/* <TextInput
+          style={styles.input5}
+          placeholder="Search your member"
+          placeholderTextColor="#032224"
+        /> */}
+      </View>
+
       <Text style={styles.font}>or</Text>
       <TextInput
-      style={styles.input}
-      placeholder="Name:"
-      placeholderTextColor="#032224"
-      secureTextEntry={false}
+        style={styles.input}
+        placeholder="Name:"
+        placeholderTextColor="#032224"
+        secureTextEntry={false}
       />
       <TextInput
-      style={styles.input}
-      placeholder="Role:"
-      placeholderTextColor="#032224"
-      secureTextEntry={false}
+        style={styles.input}
+        placeholder="Role:"
+        placeholderTextColor="#032224"
+        secureTextEntry={false}
       />
-       <TextInput
-      style={styles.input}
-      placeholder="Email:"
-      placeholderTextColor="#032224"
-      secureTextEntry={false}
+      <TextInput
+        style={styles.input}
+        placeholder="Email:"
+        placeholderTextColor="#032224"
+        secureTextEntry={false}
       />
-       <TextInput
-      style={styles.input}
-      placeholder="Phone:"
-      placeholderTextColor="#032224"
-      secureTextEntry={false}
+      <TextInput
+        style={styles.input}
+        placeholder="Phone:"
+        placeholderTextColor="#032224"
+        secureTextEntry={false}
       />
-      <TouchableHighlight
-        style={styles.button}
-      >
+      <TouchableHighlight style={styles.button}>
         <Text style={styles.buttonText}>Send Invitation</Text>
-        </TouchableHighlight>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -62,8 +80,8 @@ const styles = StyleSheet.create({
   },
   button: {
     margin: 10,
-    position: 'absolute',
-    bottom : 0,
+    position: "absolute",
+    bottom: 0,
     alignItems: "center",
     justifyContent: "center",
     width: "95%",
@@ -77,19 +95,19 @@ const styles = StyleSheet.create({
     color: "#fff",
     fontWeight: "bold"
   },
-  font:{
-    color:'#032224',
-    fontSize:40,
-    fontWeight:'bold',
-    textShadowColor:'#C0C0C0',
-    textShadowRadius:2,
-    textShadowOffset:{width:2,height:2},
+  font: {
+    color: "#032224",
+    fontSize: 40,
+    fontWeight: "bold",
+    textShadowColor: "#C0C0C0",
+    textShadowRadius: 2,
+    textShadowOffset: { width: 2, height: 2 }
   },
-  input5:{    
-    margin: 15,
+  input5: {
+    // margin: 15,
     height: 50,
     width: 280,
-    borderBottomColor: '#032224',
+    borderBottomColor: "#032224",
     borderBottomWidth: 2.4
   },
   input: {
@@ -101,4 +119,18 @@ const styles = StyleSheet.create({
     borderRadius: 0,
     paddingLeft: 10
   },
+  leftComponent: {
+    // backgroundColor: "red",
+    width: "20%",
+    // paddingBottom: 5,
+    borderBottomColor: "#000",
+    borderBottomWidth: 3,
+    alignItems: "center"
+  },
+  rightComponent: {
+    // backgroundColor: "blue",
+    width: "80%",
+    borderBottomColor: "#000",
+    borderBottomWidth: 3
+  }
 });
