082b51af602f943c972dcdb54a8caedf4cacb287
Romo <annurul.islam@bluecrossmn.com>
2020-04-12 15:46:52 -0500

    FIxed trip timeline issue

--- screens/Trips/TripTimelineScreen.js
+++ screens/Trips/TripTimelineScreen.js
@@ -1,6 +1,5 @@
-
 import React from "react";
-import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import { Calendar, CalendarList, Agenda } from "react-native-calendars";
 
 import {
   StyleSheet,
@@ -8,16 +7,18 @@ import {
   Text,
   View,
   TouchableHighlight,
-  TouchableOpacity
+  TouchableOpacity,
 } from "react-native";
-import { HeaderComponent } from "../../components/ScreenHeader";
+import { HeaderComponentWithBackButton } from "../../components/ScreenHeader";
 export default function TripTimelineScreen(props) {
-
   return (
     <View style={styles.container}>
       {/* Don't delete this: This is the header component and
        you need it to navigate to other screens */}
-      <HeaderComponent headerProps={props} screenTitle="Trip Timeline" />
+      <HeaderComponentWithBackButton
+        headerProps={props}
+        screenTitle="Trip Timeline"
+      />
 
       <Calendar
         // Initially visible month. Default = Date()
@@ -27,11 +28,15 @@ export default function TripTimelineScreen(props) {
         // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
         maxDate={undefined}
         // Handler which gets executed on day press. Default = undefined
-        onDayPress={(day) => { console.log('selected day', day) }}
+        onDayPress={(day) => {
+          console.log("selected day", day);
+        }}
         // Handler which gets executed on day long press. Default = undefined
-        onDayLongPress={(day) => { console.log('selected day', day) }}
+        onDayLongPress={(day) => {
+          console.log("selected day", day);
+        }}
         // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
-        monthFormat={'MMM yyyy'}
+        monthFormat={"MMM yyyy"}
         // Handler which gets executed when visible month changes in calendar. Default = undefined
         // Do not show days of other months in month page. Default = false
         hideExtraDays={true}
@@ -39,7 +44,9 @@ export default function TripTimelineScreen(props) {
         disableMonthChange={false}
         // Do not show days of other months in month page. Default = false
         hideExtraDays={false}
-        onMonthChange={(month) => { console.log('month changed', month) }}
+        onMonthChange={(month) => {
+          console.log("month changed", month);
+        }}
         // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed ou
         // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
         firstDay={1}
@@ -48,71 +55,76 @@ export default function TripTimelineScreen(props) {
         // Hide day names. Default = false
         hideDayNames={false}
         // Handler which gets executed when press arrow icon left. It receive a callback can go back month
-        onPressArrowLeft={substractMonth => substractMonth()}
+        onPressArrowLeft={(substractMonth) => substractMonth()}
         // Handler which gets executed when press arrow icon right. It receive a callback can go next month
-        onPressArrowRight={addMonth => addMonth()}
+        onPressArrowRight={(addMonth) => addMonth()}
         // Disable left arrow. Default = false
         disableArrowLeft={false}
         // Disable right arrow. Default = false
         disableArrowRight={false}
-
         //Marked dates
-        markingType={'custom'}
+        markingType={"custom"}
         markedDates={{
-          '2020-04-17': {
+          "2020-04-17": {
             customStyles: {
               container: {
-                backgroundColor: '#032224'
+                backgroundColor: "#032224",
               },
               text: {
-                color: 'white',
-                fontWeight: 'bold'
-              }
-            }
-          }
+                color: "white",
+                fontWeight: "bold",
+              },
+            },
+          },
         }}
         style={styles.calendar}
         theme={{
-          backgroundColor: '#ffffff',
-          calendarBackground: '#ffffff',
-          textSectionTitleColor: '#032224',
-          selectedDayBackgroundColor: 'blue',
-          selectedDayTextColor: '#032224',
-          todayTextColor: 'blue',
-          dayTextColor: '#032224',
-          textDisabledColor: '#d9e1e8',
-          dotColor: '#00adf5',
-          selectedDotColor: '#032224',
-          arrowColor: '#032224',
-          disabledArrowColor: '#d9e1e8',
-          monthTextColor: '#032224',
-          indicatorColor: '#032224',
-          textDayFontWeight: 'bold',
-          textMonthFontWeight: 'bold',
-          textDayHeaderFontWeight: 'bold',
+          backgroundColor: "#ffffff",
+          calendarBackground: "#ffffff",
+          textSectionTitleColor: "#032224",
+          selectedDayBackgroundColor: "blue",
+          selectedDayTextColor: "#032224",
+          todayTextColor: "blue",
+          dayTextColor: "#032224",
+          textDisabledColor: "#d9e1e8",
+          dotColor: "#00adf5",
+          selectedDotColor: "#032224",
+          arrowColor: "#032224",
+          disabledArrowColor: "#d9e1e8",
+          monthTextColor: "#032224",
+          indicatorColor: "#032224",
+          textDayFontWeight: "bold",
+          textMonthFontWeight: "bold",
+          textDayHeaderFontWeight: "bold",
           textDayFontSize: 16,
           textMonthFontSize: 16,
-          textDayHeaderFontSize: 15
+          textDayHeaderFontSize: 15,
         }}
       />
 
       <View style={styles.assignments}>
-        <Text style={styles.title}>
-          Assignments
-        </Text>
+        <Text style={styles.title}>Assignments</Text>
 
         <View style={styles.row}>
           <TouchableOpacity style={styles.square}></TouchableOpacity>
           <TouchableOpacity style={styles.square}></TouchableOpacity>
           <TouchableOpacity style={styles.square}>
-            <Text style={{ color: 'white', textAlign: 'center', fontSize: 17, marginTop: "12%" }}>+134</Text>
+            <Text
+              style={{
+                color: "white",
+                textAlign: "center",
+                fontSize: 17,
+                marginTop: "12%",
+              }}
+            >
+              +134
+            </Text>
           </TouchableOpacity>
         </View>
       </View>
     </View>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -130,25 +142,25 @@ const styles = StyleSheet.create({
     backgroundColor: "#032224",
     borderColor: "#032224",
     // borderWidth: 2,
-    borderRadius: 20
+    borderRadius: 20,
   },
 
   calendar: {
     borderWidth: 0,
-    borderColor: 'gray',
+    borderColor: "gray",
     height: 300,
-    width: 400
+    width: 400,
   },
 
   assignments: {
-    marginTop: 60
+    marginTop: 60,
   },
 
   title: {
     color: "#032224",
     fontWeight: "bold",
     fontSize: 20,
-    marginLeft: 20
+    marginLeft: 20,
   },
 
   row: {
@@ -156,7 +168,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     width: "100%",
     marginTop: 17,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
 
   square: {
@@ -165,6 +177,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#032224",
     marginHorizontal: 7,
     paddingVertical: 28,
-    paddingHorizontal: 0
-  }
+    paddingHorizontal: 0,
+  },
 });
